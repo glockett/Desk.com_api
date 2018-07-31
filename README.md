494015947e10f4bc4d04bb7a983a71db32c065cc
Gwyn Lockett <gwyn.lockett@theguardian.com>
2018-07-31 15:31:24 +0100

    Added a new case for stripping the text

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1>GET TOP BETA USERS FROM GMAIL - (Read me) </h1>
+<h1>GET Feedback responses for a given label - (ANA - Gen) </h1>
 <p>This is a script to pick out the Top X number of users email address from a GMail account.</p>
 <h2>Configuration</h2>
 <p>Create a config file called ""<strong>config.json</strong>".
@@ -10,13 +10,5 @@
 }
 
 
-
-Folder AppsFeedback/iOS-DailyEdition-FBack
-
-
-
-Installs
-$ sudo pip install requests
-$ pip install deskapi
 
 
--- desk.py
+++ desk.py
@@ -15,7 +15,9 @@ cf_password = data['password']
 data = BytesIO()
 
 
-url = "https://guardianuserhelp.desk.com/api/v2/labels"
+url = "https://guardianuserhelp.desk.com/api/v2/labels?per_page=1000"
+
+# url = "https://guardianuserhelp.desk.com/api/v2/cases/search?labels=\"\Windows app\""
 
 # url = "https://guardianuserhelp.desk.com/api/v2/cases"
 
@@ -24,7 +26,9 @@ url = "https://guardianuserhelp.desk.com/api/v2/labels"
 
 c = pycurl.Curl()
 c.setopt(pycurl.URL, url)
-c.setopt(pycurl.HTTPHEADER, ["Accept: application/json, sort_field=created_at&sort_direction=desc"])
+c.setopt(pycurl.HTTPHEADER, ["Accept: application/json, sort_field=created_at&sort_direction=desc, "])
+
+
 
 # # c.setopt(pycurl.HTTPHEADER, ["-H 'Accept: application/json' \
 #     -H 'Content-Type: application/json' \
@@ -34,11 +38,15 @@ c.setopt(pycurl.USERPWD, '%s:%s' % (cf_login, cf_password))
 c.setopt(c.WRITEFUNCTION, data.write)
 c.perform()
 
+
 deskData = json.loads(data.getvalue())
 
-print deskData
+print "Process Completed"
 
-#Output the data to a jSon file
+#print deskData
+
+
+#Output the data to a jSON file
 with open('deskdata.json', 'w') as outfile:
     outfile.write(json.dumps(deskData))
     outfile.close()
